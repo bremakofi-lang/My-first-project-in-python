7ae872756119fb6e9a0de78690cd11f92ab473d0
bremakofi-lang <bremakofi@gmail.com>
2026-09-01 14:51:56 +0300

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,11 @@
+# my python project
+name = "Kubi Andrea"
+print(name)
+age = 24
+print(age)
+my_favourite_language ="python"
+print(my_favourite_language)
+my_goal ="To become a software engineer"
+print(my_goal)
+introduction = "my name is kubi andrea, i am 24 years old, my favourite programming language is phython and my goal is to become a software engineer"
+print(introduction)
